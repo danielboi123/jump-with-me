fbb927cfc2776c8498e9c55be3d9b5a0b4ff2ae0
danielboi123 <153312130+danielboi123@users.noreply.github.com>
2023-12-08 09:08:20 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    game.setGameOverEffect(false, effects.melt)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.setGameOverEffect(true, effects.confetti)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (hops.vy == 0) {
+        hops.vy = -150
+    }
+})
+let hops: Sprite = null
+scene.setBackgroundColor(9)
+hops = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . f f e e e e f 2 f . . . . 
+    . . f f e e e e f 2 2 2 f . . . 
+    . . f e e e f f e e e e f . . . 
+    . . f f f f e e 2 2 2 2 e f . . 
+    . . f e 2 2 2 f f f f e 2 f . . 
+    . f f f f f f f e e e f f f . . 
+    . f f e 4 4 e b f 4 4 e e f . . 
+    . f e e 4 d 4 1 f d d e f . . . 
+    . . f e e e e e d d d f . . . . 
+    . . . . f 4 d d e 4 e f . . . . 
+    . . . . f e d d e 2 2 f . . . . 
+    . . . f f f e e f 5 5 f f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . f f . . . f f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(hops, 100, 0)
+tiles.setCurrentTilemap(tilemap`level1`)
+hops.ay = 350
+scene.cameraFollowSprite(hops)
